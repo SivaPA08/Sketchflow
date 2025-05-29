f42b6522193cf4133aea5c5b05dbbe82022526ce
Siva <sivaprasada2004@gmail.com>
2025-05-29 17:58:57 +0530

    leftside components added

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import "./styles/shapes.css"
 import Rectangle from "./comp/Rectangle";
 export default function App() {
   return (
@@ -8,7 +9,32 @@ export default function App() {
       </header>
 
       <aside className="leftbar">
-        {/* left-sidebar content */}
+        <div className="mainleft">
+        <div className="shapes">
+          <div className="title">
+            <p>Shapes</p>
+          </div>
+          <div className="contents">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint ratione et sapiente repudiandae praesentium distinctio quae. Voluptatem, amet? Ea quibusdam optio mollitia quasi id vero obcaecati corporis earum culpa consequatur?</div>
+        </div>
+
+        <div className="lines">
+          <div className="title">
+            <p>Lines</p>
+          </div>
+          <div className="contents">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum a possimus minima sunt nam quas sed. Sint, reprehenderit corporis nulla reiciendis nihil temporibus fugit eveniet possimus ratione quae, rem aut.
+          </div>
+        </div>
+
+        <div className="moreshapes">
+          <div className="title">
+            <p>More Shapes</p>
+          </div>
+          <div className="contents">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum a possimus minima sunt nam quas sed. Sint, reprehenderit corporis nulla reiciendis nihil temporibus fugit eveniet possimus ratione quae, rem aut.
+          </div>
+        </div>
+        </div>
       </aside>
 
       <main className="board">
