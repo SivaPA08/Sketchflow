9de799289ffd3d6492162d59686b6d694be30fd4
Siva <sivaprasada2004@gmail.com>
2025-06-26 18:55:31 +0530

    hover effect for dashedline done

--- src/lines/dashedline.tsx
+++ src/lines/dashedline.tsx
@@ -19,6 +19,7 @@ import {
   
   const DashedLine: FC<LineProps> = ({ left,top,bg,length:defaultLength=100,selected, onClick }) => {
     const [cord, setCord] = useState<Cord>({ x1:left, y1:top, x2:left+defaultLength, y2: top});
+    const [ishover,setishover]=useState<boolean>(false);
     const dragging = useRef<"line" | "start" | "end" | null>(null);
     const offset = useRef({ x: 0, y: 0 });
   
@@ -76,6 +77,8 @@ import {
       <>
         <div
           onMouseDown={(e) => onMouseDownPart("line", e)}
+          onMouseEnter={()=>setishover(true)}
+          onMouseLeave={()=>setishover(false)}
           style={{
             position: "absolute",
             left: midX - length / 2,
@@ -91,29 +94,35 @@ import {
   
         <div
           onMouseDown={(e) => onMouseDownPart("start", e)}
+          onMouseEnter={()=>setishover(true)}
+          onMouseLeave={()=>setishover(false)}
           style={{
             position: "absolute",
             left: cord.x1 - 8,
             top: cord.y1 - 8,
-            width: 16,
-            height: 16,
+            width: 15,
+            height: 15,
             borderRadius: "50%",
-            background: selected?"blue":"transparent",
+            background: selected || ishover?"white":"transparent",
             cursor: "pointer",
+            border:selected || ishover ?"1px solid black":"none",
           }}
         />
   
         <div
           onMouseDown={(e) => onMouseDownPart("end", e)}
+          onMouseEnter={()=>setishover(true)}
+          onMouseLeave={()=>setishover(false)}
           style={{
             position: "absolute",
             left: cord.x2 - 8,
             top: cord.y2 - 8,
-            width: 16,
-            height: 16,
+            width: 15,
+            height: 15,
             borderRadius: "50%",
-            background: selected?"blue":"transparent",
+            background: selected || ishover?"white":"transparent",
             cursor: "pointer",
+            border:selected || ishover ?"1px solid black":"none",
           }}
         />
       </>
